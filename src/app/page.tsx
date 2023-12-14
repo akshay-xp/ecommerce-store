43ff752ce465f58e7d9d05dbd6737ef70f85f7e9
Akshay PS <psakshay2022@gmail.com>
2023-12-14 18:05:41 +0530

    feat: add store front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div></div>
+import getBillboard from "@/actions/get-billboard"
+import getProducts from "@/actions/get-products"
+import ProductList from "@/components/product-list"
+import Billboard from "@/components/ui/billboard"
+import Container from "@/components/ui/container"
+
+export const revalidate = 0
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard("9c71f77e-5637-4860-846d-c91b1a2a32fd")
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
 }
+
+export default HomePage
